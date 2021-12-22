4b77f7f661d1d0cbdc7270f25f5b334791612516
TechWiz-3 <programmingplus1@gmail.com>
2021-12-22 21:50:20 +1100

    added a remind me command as well as a useless waituntil command

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import os
 import mysql.connector
+import datetime
 
 
 load_dotenv()
@@ -30,7 +31,7 @@ mydb = mysql.connector.connect(
 )
 mycursor = mydb.cursor()
 
-@bot.slash_command(guild_ids=[864438892736282625])
+@bot.slash_command(guild_ids=[864438892736282625, 867597533458202644])
 async def help(ctx):
     """Helps you use the bots commands"""
     await ctx.respond("**New Year Goal Command**\nTo use this command, type `/newyeargoal` and click space, enter or tab, then type in your goal, for best effect, type on goal at a time.")
@@ -40,15 +41,36 @@ async def hello(ctx):
     """Say hello to the bot"""  # the command description can be supplied as the docstring
     await ctx.respond(f"Hello {ctx.author}!")
 
-@bot.slash_command(guild_ids=[864438892736282625])
+@bot.slash_command(guild_ids=[864438892736282625, 867597533458202644])
 async def newyeargoal(ctx,*,goal):
     """Log a goal, one at a time"""
-    await ctx.respond(f"Yessir\nYour goal is `{goal}`\nI've logged it for you, NOW LET'S GO GET IT <:lezgooo:923128327970099231>")
+    await ctx.respond(f"Yessir\nYour goal is `{goal}`\n**I've logged it for you, NOW LET'S GO GET IT <:lezgooo:923128327970099231>**\nOh and also, remember to do `/remindme` to let me know how often to remind you about it!")
     person = str(ctx.author)
     finalValues = (person, goal)
     sql = "INSERT INTO test_goals_2002 (user, goals) VALUES (%s, %s)"
     mycursor.execute(sql, finalValues)
     mydb.commit()
-    await asyncio.sleep(2628288)
+    #await asyncio.sleep(2628288)
+
+@bot.slash_command(guild_ids=[864438892736282625, 867597533458202644])
+async def remindme(ctx,*,days):#time in days
+    await ctx.respond(f"Going to be reminding you every `{days}`\nGood job :trophy:")
+    fullTimeInSeconds = days*86400
+    while True:
+        await asyncio.sleep(fullTimeInSeconds)
+        mycursor.execute("SELECT * FROM railway ORDER BY name")
+        #mysql fetch
+        #remind person of their goals
+
+@bot.slash_command(guild_ids=[864438892736282625, 867597533458202644])
+async def wait_until(ctx, dt):
+    # sleep until the specified datetime
+    # while True:
+    now = datetime.datetime.now()
+    await ctx.respond(now)
+    #     remaining = (dt - now).total_seconds()
+    #     await asyncio.sleep(remaining)
+    #     if remaining == 0:
+    #         break
 
 bot.run(token)
