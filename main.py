3baa029817244f6ee5f7f86a08d18abba151c4ec
TechWiz-3 <programmingplus1@gmail.com>
2021-12-22 19:52:40 +1100

    added sql code and removed sql password, password has been reset so don't try hacking it

--- main.py
+++ main.py
@@ -9,20 +9,31 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import os
 import mysql.connector
-mydb = mysql.connector.connect(
-  host="containers-us-west-23.railway.app",
-  user="root",
-  password="yUWgeMOav9HmGFDqUIXo",
-  database="railway",
-  port="6499"
-)
-mycursor = mydb.cursor()
+
 
 load_dotenv()
 token = os.getenv("token")
+host = os.getenv("host")
+user = os.getenv("user")
+password = os.getenv("password")
+database = os.getenv("database")
+port = os.getenv("port")
 
 bot = commands.Bot(command_prefix=".")
 
+mydb = mysql.connector.connect(
+  host=host,
+  user=user,
+  password=password,
+  database=database,
+  port=port
+)
+mycursor = mydb.cursor()
+
+@bot.slash_command(guild_ids=[864438892736282625])
+async def help(ctx):
+    """Helps you use the bots commands"""
+    await ctx.respond("**New Year Goal Command**\nTo use this command, type `/newyeargoal` and click space, enter or tab, then type in your goal, for best effect, type on goal at a time.")
 
 @bot.slash_command(guild_ids=[864438892736282625])  # create a slash command for the supplied guilds
 async def hello(ctx):
@@ -32,6 +43,12 @@ async def hello(ctx):
 @bot.slash_command(guild_ids=[864438892736282625])
 async def newyeargoal(ctx,*,goal):
     """Log a goal, one at a time"""
-    await ctx.respond(f"Yessir\nYour goal is `{goal}`")
+    await ctx.respond(f"Yessir\nYour goal is `{goal}`\nI've logged it for you, NOW LET'S GO GET IT <:lezgooo:923128327970099231>")
+    person = str(ctx.author)
+    finalValues = (person, goal)
+    sql = "INSERT INTO test_goals_2002 (user, goals) VALUES (%s, %s)"
+    mycursor.execute(sql, finalValues)
+    mydb.commit()
+    await asyncio.sleep(2628288)
 
 bot.run(token)
